493dd5edfd2b1d2fab884245b5c40251a5e95181
a7612 <nguyencaonhan480@gmail.com>
2025-09-01 22:24:14 +0700

    1/9/2025 22:24

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 QUESTIONS_DIR = "questions"
 MAX_GENERATE_NORMAL_QUESTIONS = 20   # số câu hỏi khi chơi 1 file
-MAX_GENERATE_ALL_QUESTIONS = 50      # số câu hỏi khi chơi tất cả
+MAX_GENERATE_ALL_QUESTIONS = 10      # số câu hỏi khi chơi tất cả
 
 MAX_GENERATE_NORMAL_ANSWERS = 4   # số đáp án khi chơi 1 file
 MAX_GENERATE_ALL_ANSWERS = 4     # số đáp án khi chơi all
