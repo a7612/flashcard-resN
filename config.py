af6f30ed068cf8194c25c82f77adae281aa0adde
a7612 <nguyencaonhan480@gmail.com>
2025-11-20 15:00:07 +0700

    20/11/2025 15:00

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ MAX_GENERATE_NORMAL_ANSWERS = 1   # số đáp án khi chơi 1 file
 MAX_GENERATE_ALL_ANSWERS = 4     # số đáp án khi chơi all
 
 KEYWORD = [
-    "là gì", "bao nhiêu", "liệt kê",
+    "là gì", "bao nhiêu", "liệt kê", "id là", "chiến thuật nào",
 ]
 KEYWORD_BOOL = ["đúng hay sai"]
 
