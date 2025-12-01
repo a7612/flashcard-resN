d015562634e1daef880d41d5651e0629026c05ea
a7612 <nguyencaonhan480@gmail.com>
2025-12-01 21:50:55 +0700

    1/12/2025 21:50

--- flashcards_qiuzgame.py
+++ flashcards_qiuzgame.py
@@ -111,7 +111,7 @@ class FlashCard:
             print(f"{BRIGHT_GREEN}\n📂 Danh sách file:{RESET}")
             for i, fname in enumerate(files, 1):
                 count = self._count_questions_cached(fname)
-                print(f" {i:>2}) {fname:<25} | {count} câu hỏi")
+                print(f" {i:>2}) {fname:<25} \t({BRIGHT_CYAN}{count} {BRIGHT_RED}câu hỏi{RESET})")
         return files
 
     def _count_questions_cached(self, fname):
