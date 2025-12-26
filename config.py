3e45c7d01d1bd8c577f040437452b1f3ee9f01b9
a7612 <nguyencaonhan480@gmail.com>
2025-12-26 22:36:24 +0700

    26/12/2025 22:33

--- config.py
+++ config.py
@@ -10,10 +10,7 @@ MAX_GENERATE_NORMAL_ANSWERS = 1   # số đáp án khi chơi 1 file
 MAX_GENERATE_ALL_ANSWERS = 4     # số đáp án khi chơi all
 
 KEYWORD = [
-    "làm gì",
-    "lệnh nào", "công nghệ nào", "tệp nào", "nguyên nhân nào", "kỹ thuật tấn công nào",
-    # Tr
-    "word - what is", "translate to vietnamese",
+
 ]
 
 
