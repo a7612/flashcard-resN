3452f072873970d7c7ebc50a895f5154c9415a36
a7612 <nguyencaonhan480@gmail.com>
2026-01-04 21:22:54 +0700

    4/1/2026 21:22

--- config.py
+++ config.py
@@ -10,8 +10,8 @@ MAX_GENERATE_NORMAL_ANSWERS = 1   # số đáp án khi chơi 1 file
 MAX_GENERATE_ALL_ANSWERS = 4     # số đáp án khi chơi all
 
 KEYWORD = [
-    "bao nhiêu",
-    "thuật ngữ nào", "vị trí nào", "thuộc tính nào", "quy trình nào", "mục đích nào", "công cụ nào", "đơn vị hành chính nào", "lĩnh vực nào", "lĩnh vực chuyên sâu nào", "những lĩnh vực chính nào", "đội ngũ nào",
+    "bao nhiêu", "liệt kê",
+    "thuật ngữ nào", "vị trí nào", "thuộc tính nào", "quy trình nào", "mục đích nào", "công cụ nào", "đơn vị hành chính nào", "lĩnh vực nào", "lĩnh vực chuyên sâu nào", "những lĩnh vực chính nào", "đội ngũ nào", "đối tượng nào",
     "fill in the blank",
 ]
 
