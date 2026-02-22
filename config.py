b5b8aba62054fe7f5669f70492e69a22b66e3f99
a7612 <nguyencaonhan480@gmail.com>
2026-02-22 15:59:05 +0700

    22/02/2026 15:55

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ KEYWORD = [
     # sao
     "vì sao", "tại sao",        
     # Ai
-    "đâu là tác giả của câu nói này?", "đâu là nhân vật",
+    "đâu là tác giả hay tổ chức của câu nói này?", "đâu là nhân vật",
     # Thời gian, số
     "chính xác là bao nhiêu", "có bao nhiêu", "khoảng bao nhiêu", "được phát triển", "giai đoạn nào", "ngày, tháng nào",
     # Lệnh và thư mục
@@ -33,7 +33,7 @@ KEYWORD = [
     "translate to english",
 
     # =============
-    "đâu là đơn vị hành chính cấp xã", "địa chỉ nào",
+    "đâu là đơn vị hành chính cấp xã", "địa chỉ nào", "địa chỉ khoảng nào", "lỗ hỏng nào",
 ]
 
 
